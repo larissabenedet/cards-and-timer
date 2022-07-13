7bbd0b800ccc86d3657498da5b6a040b2a8b6b79
larissabenedet <larissavb2014@outlook.com>
2022-07-12 22:43:14 -0300

    feat: add loading title

--- src/CardsGroup.jsx
+++ src/CardsGroup.jsx
@@ -1,17 +1,18 @@
 import React, { useContext } from 'react'
+import { Heading } from 'styled-minimal'
 import Card from './components/Card'
 import { CardsContext } from './context/CardsContext'
 
 const CardsGroup = () => {
     const { cards } = useContext(CardsContext)
-    return (
-        cards && (
-            <>
-                {cards.map(({ color, timer, id }) => {
-                    return <Card color={color} timer={timer} key={id} />
-                })}
-            </>
-        )
+    return cards ? (
+        <>
+            {cards.map(({ color, timer, id }) => {
+                return <Card color={color} timer={timer} key={id} />
+            })}
+        </>
+    ) : (
+        <Heading level={2}>Carregando...</Heading>
     )
 }
 
