76092dcad1ffa00c9d210ebc9ab57b8ecb304593
larissabenedet <larissavb2014@outlook.com>
2022-07-12 22:41:30 -0300

    feat: changes global font

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -4,14 +4,15 @@ const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    font-family: 'Montserrat', Helvetica, Sans-Serif;
+    font-family: 'Playfair Display', serif, Helvetica, Sans-Serif;
+    letter-spacing: 1px;
     box-sizing: border-box;
     background-color: #F7EDDB;
   }
 
   *, html {
     box-sizing: border-box;
-  }
+}
 `
 
 export default GlobalStyle
